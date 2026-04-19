da51323198f90edea65cbb901f295ac0b0477e48
adir395 <adirmelker1@gmail.com>
2026-04-19 21:24:22 +0300

    status

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import boto3
 ec2 = boto3.client('ec2', region_name='eu-north-1')
-
-reservations = ec2.describe_instances()
-for reservation in reservations['Reservations']:
-    for instance in reservation['Instances']:
-        print(f"Instance State {instance['InstanceId']}: {instance['State']['Name']}")
+statuses = ec2.describe_instance_status()
+for status in statuses['InstanceStatuses']:
+    ins_status=status['InstanceStatus']['Status']
+    sys_status=status['SystemStatus']['Status']
+    state = status['InstanceState']['Name']
+    InstanceId = status['InstanceId']
+    print(f"Instance {InstanceId} State: {state}, Instance Status: {ins_status}, System Status: {sys_status}")
